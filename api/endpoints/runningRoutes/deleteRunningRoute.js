5620de6a5353d1949827c868bf9152a3cc634637
DerekKedrowski <dmkedrowski@gmail.com>
2024-03-06 16:08:59 -0600

    routeSaveGetDel

--- api/endpoints/runningRoutes/deleteRunningRoute.js
+++ api/endpoints/runningRoutes/deleteRunningRoute.js
@@ -1,21 +1,20 @@
 import queryMongoDatabase from "../../mongo/mongoClient.js";
 
 export default async function deleteRoute (req, res) {
+    const routeName = req.body.routeName;
     const email = req.session.email;
-    const route = req.body.route;
     const databaseName = "main";
-    const collectionName = "user";
+    const userCollectionName = "user";
+    const routeCollectionName = "routes";
     queryMongoDatabase(async db => {
-        const user = await db.collection(collectionName).findOne({ email });
+        const user = await db.collection(userCollectionName).findOne({ email });
         if (!user) {
             res.status(404).json({ message: "User not found" });
             return;
         }
-        // For the next line, I am using the updateOne method to remove the route from the savedRoutes array.
-        // This only removes the route and does not update the state of the client.
-        // Either call getSavedRoutes or use findOneAndUpdate to remove the route from the array and return the updated array.
-        const removeRoute = await db.collection(collectionName).updateOne({ email }, { $pull: { savedRoutes: { route } } }); // this needs to be updated to use the index of the route in the array
-        if (removeRoute.modifiedCount === 1) {
+        const removeRoute = await db.collection(routeCollectionName).deleteMany({ email:email , routeName:routeName  }); 
+
+        if (removeRoute.acknowledged  === true) { 
             res.status(200).json({ message: "Route deleted" });
         } else {
             res.status(500).json({ message: "Failed to delete route" });
